0976d5c6d52b88186d30aacb036bb9cf7c692d1b
EC2 Default User <ec2-user@ip-172-31-20-28.ap-northeast-1.compute.internal>
2022-11-11 14:20:56 +0000

    [Add]商品詳細画面作成

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,22 +1,27 @@
 # frozen_string_literal: true
 
 class Admin::SessionsController < Devise::SessionsController
+  
+  def after_sign_out_path_for(resource)
+    new_admin_session_path
+  end
+  
   # before_action :configure_sign_in_params, only: [:create]
 
   # GET /resource/sign_in
-  # def new
-  #   super
-  # end
+   #def new
+     #super
+   #end
 
   # POST /resource/sign_in
-  # def create
-  #   super
-  # end
+   #def create
+     #super
+   #end
 
   # DELETE /resource/sign_out
-  # def destroy
-  #   super
-  # end
+   #def destroy
+     #super
+   #end
 
   # protected
 
